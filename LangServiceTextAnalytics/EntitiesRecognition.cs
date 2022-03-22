b36a13664ad20fbb91f9e934080333fca85ce0cc
Ian Chen <ianchen0524@gmail.com>
2022-03-23 01:41:13 +0800

    TextAnalytics PII

---
+++ LangServiceTextAnalytics/EntitiesRecognition.cs
@@ -0,0 +1,24 @@
+﻿
+public class EntitiesRecognition
+{
+    public Document[] documents { get; set; }
+    public object[] errors { get; set; }
+    public string modelVersion { get; set; }
+}
+
+public class Document
+{
+    public string id { get; set; }
+    public Entity[] entities { get; set; }
+    public object[] warnings { get; set; }
+    public string redactedText { get; set; }
+}
+
+public class Entity
+{
+    public string category { get; set; }
+    public float confidenceScore { get; set; }
+    public int length { get; set; }
+    public int offset { get; set; }
+    public string text { get; set; }
+}
---
+++ LangServiceTextAnalytics/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Windows;
+
+
+namespace LangServiceTextAnalytics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void DoRecognition_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var uri = "https://{language service name}.cognitiveservices.azure.com/text/analytics/v3.1/entities/ /pii?";
+
+                //授權金鑰
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{your Subscription-Key}");
+
+
+                var queryString = HttpUtility.ParseQueryString(string.Empty);
+                //請求參數
+                queryString["domain"] = "phi";
+                queryString["piiCategories"] = "PhoneNumber,Email";
+
+                //文字內容
+                var reqModel = new ReqModel();
+                reqModel.documents = new TextContent[1];
+                reqModel.documents[0] = new TextContent()
+                {
+                    id = "1",
+                    language = "zh-hans",
+                    text = this.ReqText.Text
+                };
+
+                byte[] byteData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(reqModel, Formatting.Indented));
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    var response = await client.PostAsync(uri + queryString, content);
+                    this.EntitiesRecognition.Text = await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+    }
+}
